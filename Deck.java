d0554796799181f31240dac64d6af6fcd8ae8465
Daniel Holzner <daniel.c.holzner@gmail.com>
2024-11-18 21:14:37 +0100

    Aufgabe gelöst

--- Deck.java
+++ Deck.java
@@ -26,5 +26,79 @@ import java.util.Arrays;
  *
  */
 public class Deck {
-    
+    // create an array with all cards
+    private Card[] all_cards = new Card[0];
+
+    // TODO: Konstruktor Deck()
+    public Deck(Card[] cards) throws Exception {
+        // add the cards from another Card[] array to this deck
+        for (Card current_card: cards) {
+            this.addCard(current_card);
+        }
+    }
+
+    // TODO: Konstruktor Deck() fuer ein vollständiges Deck
+    public Deck() throws Exception {
+        // nice way to loop
+        for (Suit current_suit: Suit.values()) {
+            for (Rank current_rank: Rank.values()) {
+                Card current_card = new Card(current_suit, current_rank);
+                addCard(current_card);
+            }
+        }
+    }
+
+    // TODO: Getter
+    public Card[] getCards() {
+        // do i need to explain anything here 
+        return all_cards;
+
+    }
+
+    // TODO: addCard()
+    public void addCard(Card card) throws Exception {
+        // catch case where card is already in Deck and throw exception
+        for (int c = 0; c < all_cards.length; c++) {
+            if (card.equals(all_cards[c])) {
+                throw new Exception("Card already in deck");
+            }
+        }
+
+        // else add the card to the back of the deck
+        this.all_cards = Arrays.copyOf(this.all_cards, this.all_cards.length+1);
+        this.all_cards[this.all_cards.length-1] = card;
+    }
+
+    // TODO: pop()
+    public void pop() {
+        // just make the array shorter it'sn not that deep
+        this.all_cards = Arrays.copyOf(this.all_cards, this.all_cards.length-1);
+    }
+
+    // swap function becuase i can't figure anything out right now and i need help
+    public Card[] swap(Card[] card_list, int a, int b){
+        Card temp = card_list[a];
+        card_list[a] = card_list[b];
+        card_list[b] = temp;
+        return card_list;
+
+
+    }
+    // TODO: shuffle()
+    public void shuffle() {
+
+        // inspired by the python "shuffle" function
+        // nah but srsly why not just use Collections.shuffle -_-
+        // https://stackoverflow.com/questions/17489477/shuffle-a-python-list-without-using-the-built-in-function
+        int last_index = this.all_cards.length -1;
+        Random rnd = new Random();
+
+        while (last_index > 0) {
+        int random_index = rnd.nextInt(0, last_index);
+        this.all_cards = swap(this.all_cards, last_index, random_index);
+        last_index -= 1;
+        // idk how this works but it does
+        }
+    }
+
 }
